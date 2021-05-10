a300a98c06900a754e326660775422b68df8064f
Joonas Malinen <joonas.malinen@hotmail.fi>
2021-05-10 12:10:01 +0300

    first test

--- cp4/.ppc/cp.cu
+++ cp4/.ppc/cp.cu
@@ -10,12 +10,46 @@ This is the function you need to implement. Quick reference:
 #include <iostream>
 #include <cuda_runtime.h>
 
+__global__ void mykernel(float* result, const float* data, int nx, int ny) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+
+    for(i=i; i < ny; i++){
+        float sumI = 0;
+        float sumJ = 0;
+        float sumJI = 0;
+        float squareSumI = 0;
+        float squareSumJ = 0;    
+        for (j=i; j<ny; j++){
+            for (int x = 0; x < nx; x++){
+                sumI += data[x+i*nx];
+                sumJ += data[x+j*nx];
+                sumJI += data[x+i*nx] * data[x+j*nx];
+                squareSumI += data[x+i*nx] * data[x+i*nx];
+                squareSumJ += data[x+j*nx] * data[x+j*nx];
+            }
+        }
+        result[i + j*ny] = (size * sumJI - sumJ * sumI) 
+        / sqrt((size * squareSumJ - sumJ * sumJ) * (size * squareSumI - sumI * sumI));
+    }
+}
+
+
+static inline void check(cudaError_t err, const char* context) {
+    if (err != cudaSuccess) {
+        std::cerr << "CUDA error: " << context << ": "
+            << cudaGetErrorString(err) << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+}
+
+#define CHECK(x) check(x, #x)
 void correlate(int ny, int nx, const float *data, float *result) {
     float* dGPU = NULL;
-    CHECK(cudaMalloc((void**)&dGPU, n * n * sizeof(float)));
+    CHECK(cudaMalloc((void**)&dGPU, ny * nx * sizeof(float)));
     float* rGPU = NULL;
-    CHECK(cudaMalloc((void**)&rGPU, n * n * sizeof(float)));
-    CHECK(cudaMemcpy(dGPU, d, n * n * sizeof(float), cudaMemcpyHostToDevice));
+    CHECK(cudaMalloc((void**)&rGPU, ny * nx * sizeof(float)));
+    CHECK(cudaMemcpy(dGPU, data, ny * nx * sizeof(float), cudaMemcpyHostToDevice));
 
     // Run kernel
     dim3 dimBlock(16, 16);
@@ -27,4 +61,5 @@ void correlate(int ny, int nx, const float *data, float *result) {
     CHECK(cudaMemcpy(r, rGPU, n * n * sizeof(float), cudaMemcpyDeviceToHost));
     CHECK(cudaFree(dGPU));
     CHECK(cudaFree(rGPU));
+    
 }
