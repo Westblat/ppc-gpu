03f41a6777715b2142f2c8b829281cd7de3d2261
Joonas Malinen <joonas.malinen@hotmail.fi>
2021-05-17 16:01:29 +0300

    ??

--- cp5/cp.cu
+++ cp5/cp.cu
@@ -154,7 +154,7 @@ void correlate(int ny, int nx, const float *data, float *result) {
     CHECK(cudaGetLastError());
     }
     // Copy data back to CPU & release memory
-    CHECK(cudaMemcpy(result, rGPU, ny * ny * sizeof(float), cudaMemcpyDeviceToHost));
+    //CHECK(cudaMemcpy(result, rGPU, ny * ny * sizeof(float), cudaMemcpyDeviceToHost));
     CHECK(cudaFree(dGPU));
     CHECK(cudaFree(rGPU));
     //delete[] newData;
